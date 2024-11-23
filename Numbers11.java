88f3546f2b452d168a4f71660da03862a6f7d5fc
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-23 13:12:04 +0700

    question 5

--- Numbers11.java
+++ Numbers11.java
@@ -1,9 +1,9 @@
 public class Numbers11 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
-        myNumbers[0] = new int[5];
-        myNumbers[1] = new int[3];
-        myNumbers[2] = new int[1];
+        myNumbers[0] = new int[6];
+        myNumbers[1] = new int[6];
+        myNumbers[2] = new int[6];
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println("Length of row " + (i + 1) + ": " + myNumbers[i].length);
         }
