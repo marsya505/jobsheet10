07dd9fcd530021703ac6fbd09349df6898947f32
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-23 09:07:58 +0700

    question 11

--- Cinema11.java
+++ Cinema11.java
@@ -10,8 +10,8 @@ public class Cinema11 {
         audience[3][0] = "Gisel";
         audience[3][1] = "Hana";
         System.out.println("Audiences in the row 3: ");
-        for (int i = 0; i < audience[2].length; i++) {
-            System.out.println(audience[2][i]);
+        for (String i : audience[2]) {
+            System.out.println(i);
         }
     }   
 }
